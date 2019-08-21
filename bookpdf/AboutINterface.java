f0c291f3aeb524af88891e2d563e2a3da24d83cd
DuncantheeDuncan <mthembup69@gmail.com>
2019-08-21 08:40:40 +0200

    pushing

---
+++ bookpdf/AboutINterface.java
@@ -0,0 +1,22 @@
+class AboutInterface{
+
+
+		public interface Foo {
+int BAR = 42;
+void go();
+}
+public class Zap implements Foo {
+public void go() {
+System.out.println(BAR);
+}
+}
+
+
+public static void main(String[] args) {
+	Zap xap = new Zap();
+// AboutInterface xap = new AboutInterface();
+	xap.go();
+}
+
+
+}
